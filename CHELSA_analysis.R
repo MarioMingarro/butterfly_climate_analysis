6c28b45efe2d7b9671250fa3ec35a90315884c7c
MarioMingarro <mario_mingarro@hotmail.com>
2021-10-26 13:57:04 +0200

    new

--- CHELSA_analysis.R
+++ CHELSA_analysis.R
@@ -136,6 +136,47 @@ file.rename(paste0("B:/CHELSA_DATA/TMAX/",list.files("B:/CHELSA_DATA/TMAX")),
 
 
 # ------------------
+# Minimum temperature
+# ------------------
+
+# Load .txt with the entire directory which download
+CHELSA_dwld_paths <- readLines("CHELSA_dwld_paths_tmin.txt")
+
+# Load and reproject iberian peninsula mask
+mask <- shapefile("Data/Peninsula_Iberica_mask.shp")
+mask <- spTransform(mask, "+init=epsg:4326")
+
+# Directory to save all downloaded files
+data_rep <- "B:/CHELSA_DATA/TMIN/" 
+
+# Loop to download all files 
+for (i in 1:length(CHELSA_dwld_paths)){
+  download.file(CHELSA_dwld_paths[i],
+                dest = "raster.tif",
+                mode="wb")
+  raster <- raster("raster.tif")
+  raster <- raster %>%
+    crop(mask) %>%
+    mask(mask)
+  raster <- raster/100
+  
+  writeRaster(raster,
+              paste0(data_rep,
+                     str_sub(CHELSA_dwld_paths[i],
+                             unlist(gregexpr("min_", CHELSA_dwld_paths[i])),
+                             unlist(gregexpr("_V.2", CHELSA_dwld_paths[i])) - 1), ".tif"))
+}
+
+# Rename all files from "tas_01_1979" to "1979_01"
+file.rename(paste0("B:/CHELSA_DATA/TMAX/",list.files("B:/CHELSA_DATA/TMAX")),
+            paste0("B:/CHELSA_DATA/TMAX/",
+                   str_sub(list.files("B:/CHELSA_DATA/TMAX"), 8,11),
+                   "_",
+                   str_sub(list.files("B:/CHELSA_DATA/TMAX"), 5,6),
+                   ".tif"
+            ))
+
+
 
 ## Working with data
 # ------------------
