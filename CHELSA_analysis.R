6500ad45815f8fa6cc476262d81c383460a0fff7
MarioMingarro <mario_mingarro@hotmail.com>
2021-10-29 16:04:25 +0200

    new

--- CHELSA_analysis.R
+++ CHELSA_analysis.R
@@ -1,13 +1,12 @@
-# ------------------
+
 
 rm(list=(ls()[ls()!="v"]))
 gc(reset=TRUE)
 source("Dependencies/Functions.R")
 
 
-# ------------------
-# Download CHELSA dataset 
-# ------------------
+
+# Download CHELSA dataset ----
 
 # Monthly dataset of precipitation, maximum-, minimum-, and mean temperatures at 30 arc sec resolution for the earths land surface areas.
 # There are separate files for each month starting January 1979.
@@ -15,9 +14,7 @@ source("Dependencies/Functions.R")
 # https://envicloud.wsl.ch/#/?prefix=chelsa%2Fchelsa_V2%2FGLOBAL%2Fmonthly%2F
 
 
-# ------------------
-# Mean temperature
-# ------------------
+## Mean temperature ----
 
 # Load .txt with the entire directory which download
 CHELSA_dwld_paths <- readLines("CHELSA_dwld_paths_tmed.txt")
@@ -58,11 +55,10 @@ file.rename(paste0("B:/CHELSA_DATA/TMED/",list.files("B:/CHELSA_DATA/TMED")),
             ))
 
 
-# ------------------
-# Monthly precipitation
-# ------------------
 
-#Same steps as mean temperature (above)
+## Monthly precipitation ----
+
+# Same steps as mean temperature (above)
 CHELSA_dwld_paths <- readLines("CHELSA_dwld_paths_pcp.txt")
 
 data_rep <- "B:/CHELSA_DATA/PCP/" 
@@ -94,9 +90,8 @@ file.rename(paste0("B:/CHELSA_DATA/PCP/",list.files("B:/CHELSA_DATA/PCP")),
 
 
 
-# ------------------
-# Maximum temperature
-# ------------------
+
+## Maximum temperature ----
 
 # Load .txt with the entire directory which download
 CHELSA_dwld_paths <- readLines("CHELSA_dwld_paths_tmax.txt")
@@ -136,9 +131,8 @@ file.rename(paste0("B:/CHELSA_DATA/TMAX/",list.files("B:/CHELSA_DATA/TMAX")),
             ))
 
 
-# ------------------
-# Minimum temperature
-# ------------------
+
+## Minimum temperature ----
 
 # Load .txt with the entire directory which download
 CHELSA_dwld_paths <- readLines("CHELSA_dwld_paths_tmin.txt")
@@ -186,33 +180,44 @@ for (i in 1979:2019){
   
 }
 
-# ------------------
-## Working with data
-# ------------------
-
-# Stack all files
-datos <- raster::stack(list.files("B:/CHELSA_DATA/PCP", full.names = TRUE))
 
 
-# Monthly to annual averages (VoCC package)
-annual_data <- VoCC::sumSeries(datos, p = "1979-01/2018-12", yr0 = "1979-01-01", l = nlayers(datos), 
-               fun = function(x) colMeans(x, na.rm = TRUE), freqin = "months", freqout = "years")
 
-# PCP
+
+
+# Working with data ----
+
+## Monthly data to annual average ----
+
+
+### TMED ----
+TMED_annual <- raster::stack()
+for (i in 1979:2019){
+  raster <- calc(raster::stack(list.files("B:/CHELSA_DATA/TMED", pattern = paste0(i), full.names = TRUE)), mean)
+  TMED_annual <- raster::stack(TMED_annual, raster)
+}
+
+
+
+### PCP ----
 PCP_annual <- raster::stack()
 for (i in 1979:2018){
-  raster <- calc(raster::stack(list.files("B:/CHELSA_DATA/PCP", pattern = paste0(i), full.names = TRUE)), sum)
+  raster <- calc(raster::stack(list.files("B:/CHELSA_DATA/TMED", pattern = paste0(i), full.names = TRUE)), sum)
   PCP_annual <- raster::stack(PCP_annual, raster)
 }
 
-# Select data for specific periods
+# Calculate tmax mcal tmin mesfrio
+
+
+## Select data for specific periods ----
+
 data_1985_1989 <- raster::subset(annual_data, grep(c("1985|1986|1987|1988|1989"), names(annual_data), value = T))
 
 data_2000_2004 <- raster::subset(annual_data, grep(c("2000|2001|2002|2003|2004"), names(annual_data), value = T))
 
 data_2015_2018 <- raster::subset(annual_data, grep(c("2015|2016|2017|2018"), names(annual_data), value = T))
 
-# Calculate mean a standard deviation for diferent periods
+## Calculate mean a standard deviation for diferent periods ----
 mean_1985_1989 <- calc(data_1985_1989, mean)
 sd_1985_1989 <- calc(data_1985_1989, sd)
 
@@ -222,10 +227,10 @@ sd_2000_2004 <- calc(data_2000_2004, sd)
 mean_2015_2019 <- calc(data_2015_2019, mean)
 sd_2015_2019 <- calc(data_2015_2019, sd)
 
-# Calculate tmax mcal tmin mesfrio
 
 
-## Delete some irrelevant data and reduce RAM usage
+
+## Delete some irrelevant data and reduce RAM usage ----
 
 rm(data_1985_1989)
 rm(data_2000_2004)
@@ -235,9 +240,9 @@ rm(datos)
 
 gc(reset=TRUE)
 
-# ------------------
-## Working with the transect
-# ------------------
+
+# Working with the transect ----
+
 transect <- readOGR("Data/TRANSECTS_2021_v2.kml")
 rm(Temperature_transects_with_elevations)
 
@@ -246,7 +251,7 @@ Transects_with_elevations <- read_excel("Temperature_transects_with_elevations.x
 Transects_with_elevations <- Transects_with_elevations %>% 
   select(c(Name, Name_new, Alt, CODIGO, ZONE))
 
-# Assing zones to transects
+## Assing zones to transects ----
 ZONE <- c("GUA","GUA","GRE","GRE","GRE","GRE","GRE","GRE","GRE","GRE","GRE","GRE",
           "GRE","GRE","GRE","GRE","GUA","GUA","GUA","GUA","GUA","GUA","GUA","GUA",
           "GUA","GUA","GUA","GUA","GUA","GUA","GUA","GUA","GUA","GUA","GUA","GUA",
@@ -254,14 +259,14 @@ ZONE <- c("GUA","GUA","GRE","GRE","GRE","GRE","GRE","GRE","GRE","GRE","GRE","GRE
           "SM","SM","SM","SM","SM","SM","SM","SM","SM","SM","SM","SM","SM","SM","SM",
           "SM","JA","JA","JA","JA","JA","JA","JA","JA","JA","JA","GRE","GRE","GUA")
 
-# Get the centroids
+## Get the centroids ----
 transect_centr <- gCentroid(transect, byid = TRUE)
 transect_centr <- SpatialPointsDataFrame(transect_centr, data = transect@data)
 
 transect_centr@data <- left_join(transect_centr@data, Transects_with_elevations, by = "Name")
 view(transect_centr@data)
 
-# Extract data for each centroid
+## Extract data for each centroid ----
 transect_centr$mean_1985_1989 <- raster::extract(mean_1985_1989,
                                                  transect_centr, buffer = NULL ,exact = TRUE)
 transect_centr$sd_1985_1989 <- raster::extract(sd_1985_1989,
@@ -275,12 +280,12 @@ transect_centr$mean_2015_2019 <- raster::extract(mean_2015_2019,
 transect_centr$sd_2015_2019 <- raster::extract(sd_2015_2019,
                                                  transect_centr, buffer = NULL ,exact = TRUE)
 
-# Save data in excel
+## Save data in excel ----
 write_xlsx(transect_centr@data, "Results/Temperature_transects_results.xlsx")
 
-# ------------------
-## Some fast plots
-# ------------------
+
+# Some fast plots ----
+
 
 
 # Create a theme
@@ -373,69 +378,4 @@ ggarrange(
     theme_plot,
   legend_sd, ncol = 2, nrow = 2
 )
-
-
-## Climate velocity (NOT RUN, IN PROGRESS)
-
-# ------------------
-# Temporal trend
-vt <- tempTrend(r, th = 10)
-# Spatial gradient
-vg <- spatGrad(r, th = 0.0001, projected = FALSE)
-# Climate velocity
-gv <- gVoCC(vt, vg)
-
-plot(vel)
-plot(vg)
-plot(vt)
-
-vel <- gv[[1]] # Velocity
-ang <- gv[[2]] # Angle
-mn <- calc(r, mean, na.rm = T) # All period Temperature average
-
-# Create a data frame with the centroid for the trajectories and associated input data
-lonlat <- data.frame(xyFromCell(vel, 1:ncell(vel)))
-lonlat$vel <- raster::extract(vel, lonlat)
-lonlat$ang <- raster::extract(ang, lonlat[,1:2])
-lonlat$mn <- raster::extract(mn, lonlat[,1:2])
-lonlat <- na.omit(lonlat)
-
-
-# Calculate the trajectories with parallel processing
-cores <-  detectCores()
-ncores<- cores[1]-2 
-cuts <- cut(1:nrow(lonlat), ncores)
-cl <- makeCluster(ncores)
-tic("Execution time: ")
-registerDoParallel(cl)
-traj <- foreach(x = levels(cuts), 
-                .combine = rbind, 
-                .packages = c('raster','sp','rgeos','geosphere','rgdal','VoCC'), 
-                .multicombine = TRUE) %dopar% {
-                  voccTraj(lonlat[cuts == x,], 
-                           vel, 
-                           ang, 
-                           mn, 
-                           tyr = 47, 
-                           trajID = as.numeric(rownames(lonlat[cuts == x,])), correct = TRUE)}
-
-stopCluster(cl)
-
-toc()
-
-plot(traj)
-
-tic("Execution time: ")
-traj_cl <- trajClas(traj, vel, ang, mn, trajSt = 4, tyr = 47, nmL = 20, smL = 50, Nend = 40, Nst = 15, NFT = 70)
-toc()
-#The trajectory classes ("TrajClas") 
-#(1) non-moving, (2) slow-moving, (3) internal sinks, 
-#(4) boundary sinks, (5) sources, (6) relative sinks, 
-#(7) corridors, (8) divergence and (9) convergence.
-# ------------------
-TrajClas <- traj_cl$TrajClas
-
-TrajClas_c <- crop(TrajClas, 
-                   extent(x_min, x_max, y_min, y_max))
-
 
