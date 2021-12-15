573d4fca086d38d5799c4437787f46ed0489ef81
MarioMingarro <mario_mingarro@hotmail.com>
2021-12-15 17:02:12 +0100

    new

--- Microclima.R
+++ Microclima.R
@@ -68,7 +68,7 @@ f_fin <- data.frame(fecha_mal = ff) %>%
 
 tic("Tiempo ejecucion total: ") 
 
-for (j in 4:5){
+for (j in 6:8){
   lat <- lat_comp[j]
   long <- long_comp[j]
   mdt <- microclima::get_dem(lat = lat, long = long, resolution = 30)
