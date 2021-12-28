400ca5f4f5d78bbdd7cd3998817607141463d51e
MarioMingarro <mario_mingarro@hotmail.com>
2021-12-28 11:36:45 +0100

    new

--- Microclima_analysis.R
+++ Microclima_analysis.R
@@ -24,6 +24,39 @@ transect_centr@data <- left_join(transect_centr@data, Transects_with_elevations,
 #JAVALAMBRE
 a <- raster::stack(list.files("B:/CHELSA_DATA/JAVALAMBRE/TMAX", pattern = "x_1_" , full.names = TRUE))
 b <- raster::stack(list.files("B:/CHELSA_DATA/JAVALAMBRE/TMAX", pattern = "x_2_" , full.names = TRUE))
+
+
+test <- raster::stack()
+for (s in 1:nlayers(a)){
+  pp <- mosaic(a[[s]],
+               b[[s]],
+               fun = mean, tolerance=10000)
+  test <-  stack(test, pp)
+}
+names(test) <- names(a)
+
+source("TMM.R")
+
+kk<- melt(transect_centr_TXM@data)
+nn <- na.omit(filter(kk, kk$variable == "mean_1980_1989" | kk$variable == "mean_2009_2018" ))
+
+library(PupillometryR)
+javalambre <- ggplot(nn, aes(x= variable, y =value, fill=variable))+
+  geom_flat_violin(aes(fill = variable),position = position_nudge(x = .1, y = 0), trim = FALSE, alpha = .5, colour = NA)+
+  geom_point(aes(x = variable, y = value, colour = variable),position = position_jitter(width = .2), size = 2, shape = 20)+
+  geom_boxplot(aes(x= variable, y =value, fill=variable),outlier.shape = NA, alpha = .5, width = .1, colour = "black")+
+  scale_y_continuous("ºC",c(10, 12.5, 15, 17.5, 20,22.5,25,27.5,30), limits = c(12,30))+
+  scale_colour_brewer(palette = "Dark2")+
+  scale_fill_brewer(palette = "Dark2")+
+  ggtitle("Javalambre")+
+  theme(
+    legend.title = element_blank(),
+    axis.title.x = element_blank(),
+    axis.text.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.ticks.x = element_blank()
+  )
+
 #ALBARRACIN
 a <- raster::stack(list.files("B:/CHELSA_DATA/ALBARRACIN/TMAX", pattern = "x_1_" , full.names = TRUE))
 b <- raster::stack(list.files("B:/CHELSA_DATA/ALBARRACIN/TMAX", pattern = "x_2_" , full.names = TRUE))
@@ -78,20 +111,6 @@ plot(pp)
 nlayers()
 
 
-test <- raster::stack()
-for (s in 1:nlayers(a)){
-  pp <- mosaic(a[[s]],
-               b[[s]],
-               #c[[s]],
-               #d[[s]],
-               #e[[s]],
-               #f[[s]],
-               #g[[s]],
-               #h[[s]],
-               fun = mean, tolerance=10000)
-  test <-  stack(test, pp)
-}
-names(test) <- names(a)
 
 
 ## TXM ----
@@ -149,6 +168,13 @@ transect_centr_TXM$sd_2009_2018 <- raster::extract(TXM_sd_2009_2018,
 write_xlsx(transect_centr_TXM@data, "Results/Mean_Max_temp_transects_meridional_results.xlsx")
 
 
+
+kk<- melt(transect_centr_TXM@data)
+nn <- na.omit(filter(kk, kk$variable == "mean_1980_1989" | kk$variable == "mean_2009_2018" ))
+
+ggplot(nn, aes(x= variable, y =value, fill=variable))+
+  geom_boxplot()+
+  theme_cleveland()
 library(PupillometryR)
 javalambre <- ggplot(nn, aes(x= variable, y =value, fill=variable))+
   geom_flat_violin(aes(fill = variable),position = position_nudge(x = .1, y = 0), trim = FALSE, alpha = .5, colour = NA)+
