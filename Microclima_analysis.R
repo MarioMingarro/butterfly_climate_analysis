21004efcfa5ea6a2ac69b03d4b9e93a8deb2a5ff
MarioMingarro <mario_mingarro@hotmail.com>
2021-12-28 11:38:58 +0100

    new

--- Microclima_analysis.R
+++ Microclima_analysis.R
@@ -36,6 +36,7 @@ for (s in 1:nlayers(a)){
 names(test) <- names(a)
 
 source("TMM.R")
+write_xlsx(transect_centr_TXM@data, "Results/Mean_Max_temp_transects_javalambre_results.xlsx")
 
 kk<- melt(transect_centr_TXM@data)
 nn <- na.omit(filter(kk, kk$variable == "mean_1980_1989" | kk$variable == "mean_2009_2018" ))
@@ -112,59 +113,6 @@ nlayers()
 
 
 
-## TXM ----
-### Monthly data to annual average ----
-
-TXM_1980_1989 <- raster::stack()
-for (j in 1980:1989){ ##1980-1989, 1996-2005, 2009-2018
-  raster <- calc(raster::subset(test, grep(paste0(j), names(test), value = T)), mean)
-  TXM_1980_1989 <- raster::stack(TXM_1980_1989, raster)
-}
-names(TXM_1980_1989) <- paste0("Y_", seq(1980,1989, by = 1))
-
-TXM_1996_2005 <- raster::stack()
-for (j in 1996:2005){ ##1980-1989, 1996-2005, 2009-2018
-  raster <- calc(raster::subset(test, grep(paste0(j), names(test), value = T)), mean)
-  TXM_1996_2005 <- raster::stack(TXM_1996_2005, raster)
-}
-names(TXM_1996_2005) <- paste0("Y_", seq(1996, 2005, by = 1))
-
-TXM_2009_2018 <- raster::stack()
-for (j in 2009:2018){ ##1980-1989, 1996-2005, 2009-2018
-  raster <- calc(raster::subset(test, grep(paste0(j), names(test), value = T)), mean)
-  TXM_2009_2018 <- raster::stack(TXM_2009_2018, raster)
-}
-names(TXM_2009_2018) <- paste0("Y_", seq(2009,2018, by = 1))
-
-
-### Calculate mean a standard deviation for diferent periods ----
-TXM_mean_1980_1989 <- calc(TXM_1980_1989, mean)
-TXM_sd_1980_1989 <- calc(TXM_1980_1989, sd)
-
-TXM_mean_1996_2005 <- calc(TXM_1996_2005, mean)
-TXM_sd_1996_2005 <- calc(TXM_1996_2005, sd)
-
-TXM_mean_2009_2018 <- calc(TXM_2009_2018, mean)
-TXM_sd_2009_2018 <- calc(TXM_2009_2018, sd)
-
-
-### Extract data for each centroid ----
-transect_centr_TXM <- transect_centr
-
-transect_centr_TXM$mean_1980_1989 <- raster::extract(TXM_mean_1980_1989,
-                                                     transect_centr_TXM, buffer = NULL ,exact = TRUE)
-transect_centr_TXM$sd_1980_1989 <- raster::extract(TXM_sd_1980_1989,
-                                                   transect_centr_TXM, buffer = NULL ,exact = TRUE)
-transect_centr_TXM$mean_1996_2005 <- raster::extract(TXM_mean_1996_2005,
-                                                     transect_centr_TXM, buffer = NULL ,exact = TRUE)
-transect_centr_TXM$sd_1996_2005 <- raster::extract(TXM_sd_1996_2005,
-                                                   transect_centr_TXM, buffer = NULL ,exact = TRUE)
-transect_centr_TXM$mean_2009_2018 <- raster::extract(TXM_mean_2009_2018,
-                                                     transect_centr_TXM, buffer = NULL ,exact = TRUE)
-transect_centr_TXM$sd_2009_2018 <- raster::extract(TXM_sd_2009_2018,
-                                                   transect_centr_TXM, buffer = NULL ,exact = TRUE)
-
-write_xlsx(transect_centr_TXM@data, "Results/Mean_Max_temp_transects_meridional_results.xlsx")
 
 
 
