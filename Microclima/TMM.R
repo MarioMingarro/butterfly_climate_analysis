e75aab76db81f2b2c7d1a52174df1ad04182a7bc
MarioMingarro <mario_mingarro@hotmail.com>
2022-01-10 12:33:12 +0100

    new

--- Microclima/TMM.R
+++ Microclima/TMM.R
@@ -8,12 +8,12 @@ for (j in 1980:1989){ ##1980-1989, 1996-2005, 2009-2018
 }
 names(TXM_1980_1989) <- paste0("Y_", seq(1980,1989, by = 1))
 
-# TXM_1996_2005 <- raster::stack()
-# for (j in 1996:2005){ ##1980-1989, 1996-2005, 2009-2018
-#   raster <- calc(raster::subset(test, grep(paste0(j), names(test), value = T)), mean)
-#   TXM_1996_2005 <- raster::stack(TXM_1996_2005, raster)
-# }
-# names(TXM_1996_2005) <- paste0("Y_", seq(1996, 2005, by = 1))
+TXM_1996_2005 <- raster::stack()
+for (j in 1996:2005){ ##1980-1989, 1996-2005, 2009-2018
+  raster <- calc(raster::subset(test, grep(paste0(j), names(test), value = T)), mean)
+  TXM_1996_2005 <- raster::stack(TXM_1996_2005, raster)
+}
+names(TXM_1996_2005) <- paste0("Y_", seq(1996, 2005, by = 1))
 
 TXM_2009_2018 <- raster::stack()
 for (j in 2009:2018){ ##1980-1989, 1996-2005, 2009-2018
@@ -27,8 +27,8 @@ names(TXM_2009_2018) <- paste0("Y_", seq(2009,2018, by = 1))
 TXM_mean_1980_1989 <- calc(TXM_1980_1989, mean)
 TXM_sd_1980_1989 <- calc(TXM_1980_1989, sd)
 
-# TXM_mean_1996_2005 <- calc(TXM_1996_2005, mean)
-# TXM_sd_1996_2005 <- calc(TXM_1996_2005, sd)
+TXM_mean_1996_2005 <- calc(TXM_1996_2005, mean)
+TXM_sd_1996_2005 <- calc(TXM_1996_2005, sd)
 
 TXM_mean_2009_2018 <- calc(TXM_2009_2018, mean)
 TXM_sd_2009_2018 <- calc(TXM_2009_2018, sd)
@@ -41,10 +41,10 @@ transect_centr_TXM$mean_1980_1989 <- raster::extract(TXM_mean_1980_1989,
                                                      transect_centr_TXM, buffer = NULL ,exact = TRUE)
 transect_centr_TXM$sd_1980_1989 <- raster::extract(TXM_sd_1980_1989,
                                                    transect_centr_TXM, buffer = NULL ,exact = TRUE)
-# transect_centr_TXM$mean_1996_2005 <- raster::extract(TXM_mean_1996_2005,
-#                                                      transect_centr_TXM, buffer = NULL ,exact = TRUE)
-# transect_centr_TXM$sd_1996_2005 <- raster::extract(TXM_sd_1996_2005,
-#                                                    transect_centr_TXM, buffer = NULL ,exact = TRUE)
+transect_centr_TXM$mean_1996_2005 <- raster::extract(TXM_mean_1996_2005,
+                                                     transect_centr_TXM, buffer = NULL ,exact = TRUE)
+transect_centr_TXM$sd_1996_2005 <- raster::extract(TXM_sd_1996_2005,
+                                                   transect_centr_TXM, buffer = NULL ,exact = TRUE)
 transect_centr_TXM$mean_2009_2018 <- raster::extract(TXM_mean_2009_2018,
                                                      transect_centr_TXM, buffer = NULL ,exact = TRUE)
 transect_centr_TXM$sd_2009_2018 <- raster::extract(TXM_sd_2009_2018,
